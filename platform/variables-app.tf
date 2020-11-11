b2083360ce10ed00515018c527a1f78f7827797c
anand-swaroop-git <anandswaroop@anandmac.local>
2020-11-11 15:46:51 +1030

    Added drawing

--- platform/variables-app.tf
+++ platform/variables-app.tf
@@ -8,14 +8,5 @@ variable "app_environment" {
     description = "Application Environment"
 }
 
-variable "admin_sources_cidr" {
-  type = list(string)
-  description = "List of IPv4 CIDR blocks from which to allow admin access"
-}
-
-variable "app_sources_cidr" {
-  type = list(string)
-  description = "List of IPv4 CIDR blocks from which to allow application access"
-}
 
 
